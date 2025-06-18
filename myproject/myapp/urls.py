1729c6c07d37752ae21fc2841b540faaf0d04c22
Om Official <omstha71@gmail.com>
2025-06-18 23:55:12 +0545

    added model in server

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index , name="home"),
-    path("about/", views.about , name="about"),
+    path("<str:name>", views.index , name="home"),
+
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
-from django.http import HttpResponse 
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import ToDoList, Item 
 
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("<h1>Hello It Is Home Page</h1>")
+def index(request, name):
+    ls = ToDoList.objects.get(name=name)
+    items = ls.item_set.all()
 
-def about(request):
-    return HttpResponse("<h1>Hello It Is About Page</h1>")
+    # Build item list with status
+    item_texts = ""
+    for item in items:
+        status = "✅" if item.complete else "❌"
+        item_texts += f"{status} {item.text}<br>"
+
+    return HttpResponse(f"<h1>{ls.name}</h1><br><br><h3>{item_texts}</h3>")
+
+
+
