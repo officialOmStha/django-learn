245d7bcb73ced8fd1b43e2ce7ac952935359427d
Om Official <omstha71@gmail.com>
2025-06-19 19:59:47 +0545

    Added feature like adding item and changing state of boolean in ToDoList

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<str:name>", views.index , name="home"),
+    path("<int:id>", views.index , name="index"),
+    path("", views.home , name="home"),
+    path("create/", views.create , name="create"),
 
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,21 +1,54 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import ToDoList, Item 
-
+from .forms import CreateNewList 
 
 # Create your views here.
 
-def index(request, name):
-    ls = ToDoList.objects.get(name=name)
-    items = ls.item_set.all()
+def index(response, id):
+    ls = ToDoList.objects.get(id=id) 
 
-    # Build item list with status
-    item_texts = ""
-    for item in items:
-        status = "✅" if item.complete else "❌"
-        item_texts += f"{status} {item.text}<br>"
+    if response.method == "POST":
+        if response.POST.get("save"):
+            for item in ls.item_set.all():
+                if response.POST.get("c" + str(item.id)) == "clicked":
+                    item.complete = False
+                else:
+                    item.complete = True
+                item.save()
 
-    return HttpResponse(f"<h1>{ls.name}</h1><br><br><h3>{item_texts}</h3>")
+
+        elif response.POST.get("newItem"):
+            txt = response.POST.get("new")
+
+            if len(txt) > 2:
+                ls.item_set.create(text=txt, complete= False)
+            else:
+                print("Invalid")
+    
+
+    return render(response, "myapp/list.html", {"ls": ls})
+
+def home(response):
+    return render(response, "myapp/home.html", {})
+
+
+def create(response):
+    if response.method == "POST":
+        form = CreateNewList(response.POST)
+
+        if form.is_valid():
+            n = form.cleaned_data["name"]
+            t = ToDoList(name = n)
+            t.save()
+
+        return HttpResponseRedirect("/%i" %t.id)
+    else:
+        form = CreateNewList()
+    return render(response, "myapp/create.html", {"form":form})
+
+    
+  
 
 
 
